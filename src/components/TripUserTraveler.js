365948a81fbb620e93001f50ddc215b51599520a
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-10-25 23:48:48 -0500

    view 1. styles. fetch with general data

---
+++ src/components/TripUserTraveler.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@mui/material";
+import '../styles/TripUserTraveler.scss'
+
+function TripTraveler(props) {
+
+	const formatStops= (stops) =>{
+		const arrayStops = []
+		console.log(stops)
+		for (let key in stops){
+			arrayStops.push([key,stops[key]]);
+		}
+		return arrayStops
+	}
+	return (
+		<Card variant="outlined">
+      		<CardContent>
+				<Typography className="numTrip">{props.num}</Typography>
+				<Typography className="driver"> {props.driver}</Typography>
+				<Typography className="addrInit">PUNTO DE PARTIDA: {props.addrInit}</Typography>
+				<Typography className="addrInit">PUNTO FINAL: {props.addrFin}</Typography>
+				{formatStops(props.stops).map((stop) => <p className="stop">{stop[0] + ": " +stop[1]}</p>)}
+				<Typography className="initTime"> HORA DE SALIDA: {props.initTime}</Typography>
+			</CardContent>
+		</Card> 
+  );
+}
+
+export default TripTraveler;
---
+++ src/components/views/HomeTravelerUser.js
@@ -0,0 +1,74 @@
+import {React, useState,useEffect} from 'react'
+import "../../styles/HomeTravelerUser.scss";
+import { Grid } from "@mui/material";
+import TripTraveler from "../TripUserTraveler"
+
+function HomeTravelerUser () {
+	const [inHome, setInHome] = useState({ inHome: false });
+    const [trips, setTrips] = useState([]);
+	const [token, setToken] = useState({ token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2MzRiMDQzOWEyOTkyZjdkOGU5ZjEwNDYiLCJjbGFpbXMiOiJEUklWRVIiLCJpYXQiOjE2NjY3NTY5MjgsImV4cCI6MTY2Njc2MDUyOH0.7sL-KmTh4astg-l6x5BSI27ZVy_28h3KsJdrHMfOkpc" });
+    useEffect(() => {
+    	if (!(inHome.inHome)) {
+			try {
+				// 1. Token -> useContext (Camilo)
+				// 2. Hacer la peticion - Viajes en los que esta el usuario(Viajes activos) (IN PROCESS)
+				// 3. Peticion de viajes activos no reservados cuando le de en buscar (IN PROCESS)
+				// 4. Funcion cancelar viaje para cada uno de los viajes (IN PROCESS)
+				// 5. Mostrar informacion del conductor al dar click en el nombre (IN PROCESS)
+				
+				const idUser = '634b051b464bb818bb2e611f'
+				const requestOptionsToInfo = {
+					method: 'GET',
+					headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Authorization': 'Bearer ' + token.token}
+				}
+				//Token  y User FIN //
+				//VIAJEEEES
+				const urlTrips = 'http://localhost:8080/api/v1/trip/trips/' + idUser
+				const asyncGetTripActive = async () => {
+					try {
+						const response = await fetch(urlTrips, requestOptionsToInfo);
+						const data = await response.json().then(value => {
+							setTrips(value);
+							setInHome( { inHome: true });
+						});
+						//setTrips(data);
+						return data;
+					} catch(error) {
+							console.log(error)
+					} 
+				}
+				asyncGetTripActive();
+			}catch(error) {
+				console.log(error)
+			} 
+		}
+		else if(inHome.inHome){
+			console.log("ENTRE AL IF");
+			console.log("TRIPS render "+ trips[0].id);
+			trips.map((trip) => (console.log("TRIP " + trip.driver)))
+		}
+	}, [inHome,trips,token]);
+	//REVISAR STOPS
+    return (
+    	<div>
+				{/* NAV BAR HEEEEREEEE */}
+				<h1>👋 Hello, Welcome USER NAME!</h1>
+				<Grid className="tasks" rowSpacing={3} justifyContent="center"columnSpacing={1} container>
+					{trips.map((trip,index) => (
+						<Grid item xs="auto" wrap="nowrap" container key={trip.id}>
+							<TripTraveler
+								num = {index+1}
+								driver={trip.driver}
+								addrInit={trip.addrInit}
+								addrFin={trip.addrFin}
+								initTime={trip.initTime}
+								stops = {trip.stops}
+							></TripTraveler>
+						</Grid>
+					))}
+				</Grid>
+		</div>
+    );
+}
+
+export default HomeTravelerUser;
